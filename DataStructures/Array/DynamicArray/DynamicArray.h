27af2070a19d50cd791e4aecda5f305246ed2aab
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2021-10-28 23:23:14 +0630

    Copy and move constructor and assignment operator

--- DataStructures/Array/DynamicArray/DynamicArray.h
+++ DataStructures/Array/DynamicArray/DynamicArray.h
@@ -21,14 +21,14 @@ private:
 
 	#pragma mark Validator
 	// check and throw if the index is invalid i.e out of valid range
-	void throw_if_invalid_indexing(int index) {
+	void throw_if_invalid_indexing(int index) const {
 		// if the index out of bound, throw exception
 		if (index < 0 or index >= array_size) 
 			throw std::out_of_range("Array index out of bound");
 	}
 
 	// check if the current array has reach it's capacity
-	bool is_capacity_reached() {
+	bool is_capacity_reached() const noexcept {
 		return size() == capacity();
 	}
 
@@ -92,6 +92,76 @@ public:
 		std::copy(list.begin(), list.end(), this->elements);
 	}
 
+	#pragma mark Copy Constructor
+	// Copy everything from the another dynamic array.
+	DynamicArray(const DynamicArray<Element>& array):
+	// allocate, zero initialize
+	DynamicArray(array.size())
+	{
+		// copy everything
+		for(int i = 0; i < array.size(); i++)
+			this->elements[i] = array[i];
+	}
+
+	#pragma mark Copy Assignment
+	// Copy Assignment
+	virtual DynamicArray& operator=(const DynamicArray<Element>& rvalue) {
+		// if the current array's capacity
+		// is not enough to copy everything from the rvalue
+		if (capacity() < rvalue.capacity()) {
+			// deallocate the elements
+			delete[] this->elements;
+			// change the capacity
+			array_capacity = rvalue.capacity();
+			// and reallocate new elements capacity
+			this->elements = new Element[rvalue.capacity()];
+		}
+
+		// change the size
+		array_size = rvalue.size();
+
+		// copy everything from the rvalue
+		for(int i = 0; i < rvalue.size(); i++)
+			this->elements[i] = rvalue[i];
+
+		// and return current object.
+		return *this;
+	}
+
+
+	#pragma mark Move
+	// Move constructor
+	DynamicArray(DynamicArray<Element>&& array) {
+		// steal properties
+		this->array_capacity = array.capacity();
+		this->array_size = array.size();
+		this->elements = array.elements;
+
+		// invalidate rvalue
+		// so that the pointer wont get deleted when the
+		// rvalue is deallocated.
+		array.array_capacity = 0;
+		array.array_size = 0;
+		array.elements = nullptr;
+	}
+
+	#pragma mark Move Constructor
+	// Move assignment
+	virtual DynamicArray<Element>& operator=(DynamicArray<Element>&& rvalue) {
+		this->array_capacity = rvalue.capacity();
+		this->array_size = rvalue.size();
+		this->elements = rvalue.elements;
+
+		// invalidate rvalue
+		// so that the pointer wont get deleted when the
+		// rvalue is deallocated.
+		rvalue.array_capacity = 0;
+		rvalue.array_size = 0;
+		rvalue.elements = nullptr;
+		return *this;
+	}
+
+
 	#pragma mark Destructor
 	~DynamicArray() {
 		// delete the elements.
@@ -112,14 +182,21 @@ public:
 	}
 
 	// Return the current size of the array.
-	const size_t size() const { 
+	size_t size() const noexcept { 
 		return array_size; 
 	}
 
 	// Size of the memory allocated by the dynamic array.
-	const size_t capacity() const {
+	size_t capacity() const noexcept {
 		return array_capacity;
 	}
+
+	// test if the array is empty
+	bool is_empty() const noexcept {
+		// if the size is zero, it's true
+		return not size();
+	}
+
 
 	#pragma mark Modifiers
 	void prepend(const Element &item) {
--- DataStructures/Array/DynamicArray/test.cpp
+++ DataStructures/Array/DynamicArray/test.cpp
@@ -3,26 +3,35 @@
 #include <iostream>
 using namespace std;
 
+void print(const DynamicArray<int>& array) {
+	for(int i = 0; i < array.size(); i++) 
+		cout << array[i] << " ";
+	cout << endl;
+}
+
 int main() {
 	DynamicArray<int> a(2, 100);
-
+	// copy test
+	DynamicArray<int> c = a;
 	// append test
 	for(int i = 0; i < 10; i++) a.append(i);
-	for(int i = 0; i < a.size(); i++) 
-		cout << a[i] << " ";
-	cout << endl;
+	print(a);
 
 	// prepend test
 	for(int i = -1; i > -11; i--) a.prepend(i);
-	for(int i = 0; i < a.size(); i++) 
-		cout << a[i] << " ";
-	cout << endl;
+	print(a);
+	print(c);
+
+	// Move constructor test;
+	DynamicArray<int> d = std::move(c);
+
+	print(c);
+	print(a);
 
 	// initializer list test
 	DynamicArray<int> b = {1, 2, 3, 4, 5};
-	for(int i = 0; i<b.size(); i++) {
-		cout << b[i] << " ";
-	}
-	cout << endl;
+	print(b);
+
+
 	return 0;
 }
