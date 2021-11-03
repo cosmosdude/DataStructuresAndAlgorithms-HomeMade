795851625e346015c8dcc79cd5f050f91e1eb381
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2021-11-03 16:15:35 +0630

    introduce bidirectional node

--- DataStructures/LinkedList/Node.h
+++ DataStructures/LinkedList/Node.h
@@ -10,4 +10,15 @@ struct ForwardNode {
 	element(e), next(nullptr) {}
 };
 
+template<typename T>
+struct BidirectionalNode {
+	T element;
+	BidirectionalNode<T> *previous;
+	BidirectionalNode<T> *next;
+
+	BidirectionalNode(const T& e):
+	element(e), next(nullptr), previous(nullptr)
+	{}
+}
+
 #endif
